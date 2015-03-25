6fc3cd4ae7492762fd2eb3c46fdcb5020e834fab
jianshen_0 <jianshen@dev136002.sqa.cm4>
2015-03-25 14:53:09 +0800

    update secondarysort

--- hadoop/hadoop2/wordcount/src/main/java/secondarysort/DataPartitioner.java
+++ hadoop/hadoop2/wordcount/src/main/java/secondarysort/DataPartitioner.java
@@ -6,11 +6,11 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class DataPartitioner extends Partitioner<StringPair, Text> {
 
     @Override
-    public int getPartition(StringPair key, Text val, int arg2) {
+    public int getPartition(StringPair key, Text val, int numPartitions) {
         String strKey = key.getFirst();
         try {
             long lKey = Long.parseLong(strKey);
-            return (int)(lKey % arg2);
+            return (int)(lKey % numPartitions);
         }catch(Exception e) {
             throw new RuntimeException(e);
         }
