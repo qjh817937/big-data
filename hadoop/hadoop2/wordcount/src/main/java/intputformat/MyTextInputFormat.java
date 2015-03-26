83d23695f73ccd98f86674fd840c86a54a7f3080
jianshen_0 <jianshen@dev136002.sqa.cm4>
2015-03-26 11:01:53 +0800

    update inputformat

---
+++ hadoop/hadoop2/wordcount/src/main/java/intputformat/MyTextInputFormat.java
@@ -0,0 +1,51 @@
+package intputformat;
+
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.classification.InterfaceAudience;
+import org.apache.hadoop.classification.InterfaceStability;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionCodecFactory;
+import org.apache.hadoop.io.compress.SplittableCompressionCodec;
+import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+import com.google.common.base.Charsets;
+
+/** An {@link InputFormat} for plain text files.  Files are broken into lines.
+ * Either linefeed or carriage-return are used to signal end of line.  Keys are
+ * the position in the file, and values are the line of text.. */
+@InterfaceAudience.Public
+@InterfaceStability.Stable
+public class MyTextInputFormat extends FileInputFormat<LongWritable, Text> {
+
+  @Override
+  public RecordReader<LongWritable, Text> 
+    createRecordReader(InputSplit split,
+                       TaskAttemptContext context) {
+    //String delimiter = context.getConfiguration().get(
+        //"textinputformat.record.delimiter");
+    //String delimiter = "\u0001";
+    //String delimiter = "\u001E";
+    byte[] recordDelimiterBytes =  new byte[1];
+    recordDelimiterBytes[0] = (byte) 0x1e;
+    return new LineRecordReader(recordDelimiterBytes);
+  }
+
+  @Override
+  protected boolean isSplitable(JobContext context, Path file) {
+    final CompressionCodec codec =
+      new CompressionCodecFactory(context.getConfiguration()).getCodec(file);
+    if (null == codec) {
+      return true;
+    }
+    return codec instanceof SplittableCompressionCodec;
+  }
+
+}
+
